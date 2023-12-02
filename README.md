27c8fed2401dd406e63dad917acfd606a5c91adb
pedro05barata <pedro.barata.05@gmail.com>
2023-12-02 03:55:32 +0000

    x

--- README.md
+++ README.md
@@ -1,5 +1,6 @@
 # Youtube-Bookmark-Extention
 #Hey! Here you can find an open source code for a Youtube BookMark extension wrote in JavaScript, HTML and CSS.
+This code allows you to save time stamps in Youtube Videos. The program can do this by recognizing the video you're watching and memorizing your time stamps. 
 
 
 
--- popup.js
+++ popup.js
@@ -1,81 +1,86 @@
-// Import necessary utility function from the "utils.js" file
+// Importing the getActiveTabURL function from the utils.js file
 import { getActiveTabURL } from "./utils.js";
 
-// Function to create a new bookmark element and append it to the bookmarks container
-const createBookmarkElement = (bookmark) => {
+// Function to add a new bookmark to the bookmarks container
+const addNewBookmark = (bookmarks, bookmark) => {
+  // Creating elements for the bookmark
   const bookmarkTitleElement = document.createElement("div");
   const controlsElement = document.createElement("div");
   const newBookmarkElement = document.createElement("div");
 
-  // Set the bookmark title and classes
+  // Setting properties for the bookmark elements
   bookmarkTitleElement.textContent = bookmark.desc;
   bookmarkTitleElement.className = "bookmark-title";
   controlsElement.className = "bookmark-controls";
 
-  // Add play and delete controls with event listeners
+  // Adding play and delete controls to the bookmark
   setBookmarkAttributes("play", onPlay, controlsElement);
   setBookmarkAttributes("delete", onDelete, controlsElement);
 
-  // Set attributes and append child elements
-  newBookmarkElement.id = `bookmark-${bookmark.time}`;
+  // Setting ID, class, and timestamp attribute for the new bookmark element
+  newBookmarkElement.id = "bookmark-" + bookmark.time;
   newBookmarkElement.className = "bookmark";
   newBookmarkElement.setAttribute("timestamp", bookmark.time);
+
+  // Appending bookmark elements to the new bookmark element
   newBookmarkElement.appendChild(bookmarkTitleElement);
   newBookmarkElement.appendChild(controlsElement);
-
-  return newBookmarkElement;
+  bookmarks.appendChild(newBookmarkElement);
 };
 
-// Function to render bookmarks in the bookmarks container
-const renderBookmarks = (bookmarks, currentBookmarks = []) => {
-  bookmarks.innerHTML = "";
+// Function to display bookmarks in the bookmarks container
+const viewBookmarks = (currentBookmarks = []) => {
+  const bookmarksElement = document.getElementById("bookmarks");
+  bookmarksElement.innerHTML = "";
 
+  // Checking if there are bookmarks to display
   if (currentBookmarks.length > 0) {
-    // Loop through current bookmarks and append them to the container
-    currentBookmarks.forEach((bookmark) => {
-      const bookmarkElement = createBookmarkElement(bookmark);
-      bookmarks.appendChild(bookmarkElement);
-    });
+    for (let i = 0; i < currentBookmarks.length; i++) {
+      const bookmark = currentBookmarks[i];
+      addNewBookmark(bookmarksElement, bookmark);
+    }
   } else {
-    // Display a message if there are no bookmarks
-    bookmarks.innerHTML = '<i class="row">No bookmarks to show</i>';
+    // Displaying a message if there are no bookmarks
+    bookmarksElement.innerHTML = '<i class="row">No bookmarks to show</i>';
   }
+
+  return;
 };
 
-// Event handler for the "Play" button
+// Function to handle the play action on a bookmark
 const onPlay = async (e) => {
-  const bookmarkTime = e.target.closest(".bookmark").getAttribute("timestamp");
+  const bookmarkTime = e.target.parentNode.parentNode.getAttribute("timestamp");
   const activeTab = await getActiveTabURL();
 
-  // Send a message to the active tab to play the video at the bookmarked time
+  // Sending a message to the active tab to play the video from the bookmarked time
   chrome.tabs.sendMessage(activeTab.id, {
     type: "PLAY",
     value: bookmarkTime,
   });
 };
 
-// Event handler for the "Delete" button
+// Function to handle the delete action on a bookmark
 const onDelete = async (e) => {
   const activeTab = await getActiveTabURL();
-  const bookmarkTime = e.target.closest(".bookmark").getAttribute("timestamp");
-  const bookmarkElementToDelete = document.getElementById(`bookmark-${bookmarkTime}`);
+  const bookmarkTime = e.target.parentNode.parentNode.getAttribute("timestamp");
+  const bookmarkElementToDelete = document.getElementById("bookmark-" + bookmarkTime);
 
-  // Remove the bookmark element from the DOM
-  bookmarkElementToDelete?.remove();
+  // Deleting the bookmark element from the DOM
+  bookmarkElementToDelete.parentNode.removeChild(bookmarkElementToDelete);
 
-  // Send a message to the active tab to delete the bookmark
+  // Sending a message to the active tab to delete the bookmarked time
   chrome.tabs.sendMessage(activeTab.id, {
     type: "DELETE",
     value: bookmarkTime,
-  }, () => renderBookmarks(bookmarksElement, currentBookmarks));
+  }, viewBookmarks);
 };
 
-// Function to set attributes for the play and delete controls
+// Function to set attributes for a bookmark control element
 const setBookmarkAttributes = (src, eventListener, controlParentElement) => {
   const controlElement = document.createElement("img");
 
-  // Set control attributes and add event listener
-  controlElement.src = `assets/${src}.png`;
+  // Setting properties for the control element
+  controlElement.src = "assets/" + src + ".png";
   controlElement.title = src;
   controlElement.addEventListener("click", eventListener);
   controlParentElement.appendChild(controlElement);
@@ -83,24 +88,26 @@ const setBookmarkAttributes = (src, eventListener, controlParentElement) => {
 
 // Event listener for the DOMContentLoaded event
 document.addEventListener("DOMContentLoaded", async () => {
-  // Get the active tab and extract video parameters from the URL
+  // Getting the URL of the active tab and extracting video parameters
   const activeTab = await getActiveTabURL();
   const queryParameters = activeTab.url.split("?")[1];
   const urlParameters = new URLSearchParams(queryParameters);
 
+  // Getting the current video ID from the URL
   const currentVideo = urlParameters.get("v");
 
-  // Check if the active tab is a YouTube video page
+  // Checking if the active tab is a YouTube video page
   if (activeTab.url.includes("youtube.com/watch") && currentVideo) {
-    // Retrieve stored bookmarks for the current video
+    // Retrieving bookmarks for the current video from storage
     chrome.storage.sync.get([currentVideo], (data) => {
       const currentVideoBookmarks = data[currentVideo] ? JSON.parse(data[currentVideo]) : [];
-      // Render bookmarks in the container
-      renderBookmarks(bookmarksElement, currentVideoBookmarks);
+
+      // Displaying the bookmarks on the page
+      viewBookmarks(currentVideoBookmarks);
     });
   } else {
-    // Display a message if the page is not a YouTube video page
-    const container = document.querySelector(".container");
+    // Displaying a message if the active tab is not a YouTube video page
+    const container = document.getElementsByClassName("container")[0];
     container.innerHTML = '<div class="title">This is not a YouTube video page.</div>';
   }
 });
